a801b0576033b1d5deb9f22749e6a8a67bfd5a0c
shubhi.ag09@gmail.com <shubhi.ag09@gmail.com>
2022-01-21 15:44:40 +0530

    committing

---
+++ todo_list/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.home,name="home"),
+    path('delete/<list_id>',views.delete,name="delete"),
+    path('done/<list_id>',views.done,name="done"),
+    path('not_done/<list_id>',views.not_done,name="not_done"),
+    path('edit/<list_id>',views.edit,name="edit"),
+]
