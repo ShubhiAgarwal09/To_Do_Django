03a3091607f9f82e30f3c60243bc66e115e8bd47
shubhi.ag09@gmail.com <shubhi.ag09@gmail.com>
2022-01-21 21:31:13 +0530

    firstcommit

--- todo/settings.py
+++ todo/settings.py
@@ -125,9 +125,9 @@ STATIC_URL = '/static/'
 # ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
- STATICFILES_DIRS = (
-     os.path.join(BASE_DIR, 'static'),
- )
+#  STATICFILES_DIRS = (
+#      os.path.join(BASE_DIR, 'static'),
+#  )
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
