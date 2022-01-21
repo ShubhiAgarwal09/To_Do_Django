fb0c4ace3a3bd9feae5f95175e5690dcc2eb7faa
shubhi.ag09@gmail.com <shubhi.ag09@gmail.com>
2022-01-21 23:00:00 +0530

    coommiitt

--- todo/settings.py
+++ todo/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
@@ -138,4 +138,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
