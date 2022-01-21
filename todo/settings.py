b9e47eab2198775bb468383d3c2904e18dc9c1f4
shubhi.ag09@gmail.com <shubhi.ag09@gmail.com>
2022-01-21 16:09:18 +0530

    secondcommit

--- todo/settings.py
+++ todo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,5 +125,11 @@ STATIC_URL = '/static/'
 # ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
