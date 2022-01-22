8b20c3fb15d58db7a4e50c6e8cb0ce0a96189d38
shubhi.ag09@gmail.com <shubhi.ag09@gmail.com>
2022-01-22 14:44:10 +0530

    edited

--- todo/urls.py
+++ todo/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path,include
 from todo_list import views
 
-from django.views.static import serve
+from django.views import generic
 # from django.conf.urls import url
 
 urlpatterns = [
+    #path('',include('todo_list.urls')), 
+    path("",generic.TemplateView.as_view(template_name="todo_list.urls")), 
     path('admin/', admin.site.urls),
-    path('',include('todo_list.urls')), 
+    
 
     # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
